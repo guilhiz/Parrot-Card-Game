3f767342d213f3130b0b06a00f3261c44aa3fb8f
guilhiz <guilhermevilelasobral@gmail.com>
2022-10-20 22:16:54 -0300

    finished project

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -2,6 +2,9 @@ const containerCard = document.querySelector(".container-card");
 const numberCardsSelected = [];
 let cardInnerHtml = "";
 let firstCard, secondCard;
+let cardBlockFlip = false;
+let qtdCardsClicked = 0;
+let qtdCardClickedEqual = 0;
 const cardListGif = [
   "bobrossparrot.gif",
   "unicornparrot.gif",
@@ -13,7 +16,7 @@ const cardListGif = [
 ];
 
 function numberOfCards() {
-  const numberCard = prompt("Choose the number of cards between 4 and 14");
+  let numberCard = prompt("Choose the number of cards between 4 and 14");
   const dividingCardNumbers = numberCard / 2;
   //Return the prompt if the number of cards is higher than 14, fewer than 4, or an odd number
   if (numberCard > 14 || numberCard < 4 || numberCard % 2 != 0) {
@@ -25,7 +28,6 @@ function numberOfCards() {
       numberCardsSelected.push(cardListGif[i]);
       numberCardsSelected.push(cardListGif[i]);
     }
-
     gameInit();
   }
 }
@@ -58,19 +60,65 @@ function gameInit() {
 }
 
 function flipCard(selected) {
+  if (cardBlockFlip == true) return false;
   const backCard = selected.querySelector(".back-card");
-  selected.classList.toggle("flip");
-  backCard.classList.toggle("hidden");
+  selected.classList.add("flip");
+  backCard.classList.add("hidden");
   if (firstCard == undefined) {
     firstCard = selected;
-    return false
+    return false;
   }
-  secondCard = selected
- checkcards()
+  if (selected !== firstCard) {
+    secondCard = selected;
+    checkcards();
+  }
 }
 
 function checkcards() {
   let check = firstCard.dataset.set === secondCard.dataset.set;
-  console.log(check)
+  if (check == false) {
+    qtdCardsClicked++;
+
+    removeFlip();
+  } else {
+    firstCard.classList.add("disabled")
+    secondCard.classList.add("disabled")
+    firstCard.onclick = ""
+    secondCard.onclick = ""
+    qtdCardsClicked++;
+    //testar se ! tá retornando false
+    // if (!firstCard.classList.contains("disabled") || !secondCard.classList.contains("disabled")) {
+      qtdCardClickedEqual++;
+      
+      console.log(qtdCardClickedEqual)
+    // }
+    
+   if (qtdCardClickedEqual == numberCardsSelected.length/2) {
+    setTimeout(() => {
+      alert(`Você ganhou em ${qtdCardsClicked} jogadas!`)
+    }, 1000)
+   }
+    reset();
+  }
+}
+
+function removeFlip() {
+  cardBlockFlip = true;
+  const firstBackCard = firstCard.querySelector(".back-card");
+  const secondBackCard = secondCard.querySelector(".back-card");
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    firstBackCard.classList.remove("hidden");
+    secondCard.classList.remove("flip");
+    secondBackCard.classList.remove("hidden");
+
+    reset();
+  }, 1000);
+}
+
+function reset() {
+  cardBlockFlip = false;
+  firstCard = null;
+  secondCard = null;
 }
 numberOfCards();
